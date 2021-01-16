ad3c5bc05d4325a5f5fce64635bb5751abaf5de0
Danny Tomkins <tomkinsdan@gmail.com>
2021-01-15 19:25:04 -0600

    attempted to update crud functions in orm.js and burgers_controller.js; not working, more work needed.

--- config/orm.js
+++ config/orm.js
@@ -11,15 +11,78 @@ function printQuestionMarks(num) {
     return arr.toString();
     }
 
+//helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+
+    //loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        //check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+        //if string with spaces, add quotations
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+            value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+        }
+    }
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
+
 
 
 const burgers_orm = {
+    //selectAll
     selectAll: function(tableName, cbModel){
         connection.query("SELECT * FROM ?? ", tableName, function(error, resultsdb){
             cbModel(resultsdb)
         } )
     },
+    //insertOne
+    insertOne: function(table, cols, vals, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (";
+        queryString += printQuestionMarks(vals.length);
+        queryString += ") ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+            throw err;
+            }
+    
+            cb(result);
+        });
+    },
+    //updateOne
+    updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+        if (err) {
+        throw err;
+        }
+
+        cb(result);
+    });
+    }
 }
+    
+
 
 //export orm object
 module.exports = burgers_orm
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,16 +1,43 @@
 const burgers_orm = require("../config/orm")
+//import the model to use database function
 const burgers_model = require("../models/burger")
 
 const router = require("express").Router()
-
+//get method
 router.get("/", function(req, res){
     burgers_model.selectAll(function(resultsdb){
         res.render("index", {burgers:resultsdb})
     })
     
 })
+//post method
+router.post("/api/burgers", function(req, res) {
+    burger.create(["burger_name"], [req.body.name], function(result) {
+      // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+    });
+    });
+//put, update method
+router.put("/api/burgers/:id", function(req, res) {
+    var devoured = "id = " + req.params.id;
+
+    console.log("devoured", devoured);
+
+    burger.update(
+        {
+        devoured: req.body.devoured
+        },
+        condition,
+        function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+            return res.status(404).end();
+        }
+        res.status(200).end();
+        }
+    );
+});
 
 
-
-
+//export routes fore server.js to use
 module.exports = router
